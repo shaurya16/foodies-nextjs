818c2442b654a07449a16fcfa960e8a2fe38fad2
Shaurya Srivastava <shaurya16@gmail.com>
2025-12-12 00:23:33 +0800

    Add MealsGrid and SqliteDB

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,11 @@
 import Link from "next/link";
 import classes from "./page.module.css";
+import MealsGrid from "../components/meals/meals-grid";
+import { getMeals } from "../lib/meals";
 
-export default function MealsPage() {
+export default async function MealsPage() {
+  const mealsData = await getMeals();
+
   return (
     <>
       <header className={classes.header}>
@@ -16,7 +20,9 @@ export default function MealsPage() {
           <Link href="/meals/share">Share your Favorite Recipe</Link>
         </p>
       </header>
-      <main className={classes.main}></main>
+      <main className={classes.main}>
+        <MealsGrid meals={mealsData} />
+      </main>
     </>
   );
 }
