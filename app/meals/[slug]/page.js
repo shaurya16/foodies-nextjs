5b1a49bb2d6b62ea7b74ec64e8bd990e3d31f3c7
Shaurya Srivastava <shaurya16@gmail.com>
2025-12-11 22:52:28 +0800

    initial commit

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,3 @@
+export default function MealDetails({ params }) {
+  return <h1>Meal Details</h1>;
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,7 @@
+export default function MealsSharePage() {
+  return (
+    <main>
+      <h1>Meals Share</h1>
+    </main>
+  );
+}
